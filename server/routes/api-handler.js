00c7804dd3bb8a4c7184ffa2d1dae9244ccaa1f8
liupp <liupp@2345.com>
2018-09-07 10:25:08 +0800

    youhua

--- server/routes/api-handler.js
+++ server/routes/api-handler.js
@@ -1,26 +1,31 @@
 var operateMysql = require('../mysql/index.js')
 module.exports = function (req, res, next) {
   const {url} = req
+  console.log(url)
   operateMysql('select * from apilist where path=?', url, function (err, result) {
     res.set({
       'Access-Control-Allow-Origin': '*',
       'Cache-Control': 'no-store',
       'Access-Control-Allow-Headers': 'x-requested-with, content-type'
     })
+    if (err) console.log(err)
     if (result.length) {
-      let json;
+      let json = result[0].json;
       try {
-        json = result[0].json
-          .replace(/\s*\u21b5\s*/g, '')
-          .replace(/\s*([:,{}])\s*/g, '$1')
-          .replace(/('|")?([^'"{}:,]+)('|")?/g, '"$2"')
         json = JSON.parse(json)
       } catch (err) {
-        json = result[0].json
+        try {
+          json = json
+            .replace(/\s*\u21b5\s*/g, '')
+            .replace(/('|")([^'"]+)('|")/g, '"$2"')
+          json = JSON.parse(json)
+        } catch (err) {
+          json = result[0].json
+        }
       }
       res.send(json)
     } else {
-      next()
+      res.send('未找到对应页面或接口！')
     }
   })
 }
